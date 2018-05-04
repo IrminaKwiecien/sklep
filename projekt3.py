6e7e06f5274b4be28e42718c7bf64d520f2baf1c
anieweglowska <33904850+anieweglowska@users.noreply.github.com>
2018-05-04 09:58:16 +0200

    Poprawki

--- projekt3.py
+++ projekt3.py
@@ -29,9 +29,11 @@ class test(unittest.TestCase):
         cookies_info.click()
         sleep(1)
         addToBasket = driver.find_element_by_id("add2BasketBtn")
+        driver.execute_script("window.scrollTo(0, 30)")
         addToBasket.click()
         sleep(1)
         discount =driver.find_element_by_xpath('//*[@id="pcode"]')
+        driver.execute_script("window.scrollTo(0, 300)")
         discount.click()
         discount.send_keys(invalid_discount)
         sleep(1)
@@ -41,16 +43,12 @@ class test(unittest.TestCase):
         sleep(5)
         modalcontent = driver.find_element_by_css_selector(".nyroModalIframe")
         frames = modalcontent.find_elements_by_css_selector("iframe")
-
         assert len(frames) == 1
-
         frame = frames[0]
         driver.switch_to.frame(frame)
         error = driver.find_element_by_css_selector("h1")
         assert error.text == u"BŁĄD: KOD NIEPOPRAWNY"
-
-
-
+        sleep(3)
 
     def tearDown(self):
         self.driver.quit()
