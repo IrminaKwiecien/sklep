2cc04fda5ca79e148a10d52728b8d8a8906d071a
anieweglowska <33904850+anieweglowska@users.noreply.github.com>
2018-05-08 19:26:57 +0200

    Wersja ostateczna projektu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+
 
 valid_name = "Anna"
 valid_surname = "Nowak"
@@ -24,62 +24,45 @@ class test(unittest.TestCase):
         self.driver.maximize_window()
         self.driver.get("https://www.sukienkimm.pl")
 
-
-
-
     def test_exaple(self):
         driver = self.driver
         login_shop=driver.find_element_by_class_name("loginIco")
         login_shop.click()
-        sleep(2)
         cookies_info = driver.find_element_by_id("close-cookie-info")
         cookies_info.click()
-        sleep(2)
+        sleep(1)
         regestration_shop = driver.find_element_by_class_name('xlarge')
         regestration_shop.click()
-        sleep(2)
         name_user = driver.find_element_by_id('usr_first_name')
         name_user.send_keys(valid_name)
-        sleep(1)
         surname_user = driver.find_element_by_id("usr_last_name")
         surname_user.send_keys(valid_surname)
-        sleep(1)
         street_user = driver.find_element_by_id("usr_address")
         street_user.send_keys(valid_street)
-        sleep(1)
         number_user = driver.find_element_by_id("usr_address_str_no")
         number_user.send_keys(valid_number)
-        sleep(1)
         code_user = driver.find_element_by_id("usr_post_code")
         code_user.send_keys(valid_code)
-        sleep(1)
         city_user = driver.find_element_by_id("usr_city")
         city_user.send_keys(valid_city)
-        sleep(1)
         email_user = driver.find_element_by_id("usr_email")
         email_user.send_keys(valid_email)
-        sleep(1)
         password_user = driver.find_element_by_id("usr_password")
         password_user.send_keys(valid_password)
-        sleep(1)
         password2_user = driver.find_element_by_id("usr_password_confirm")
         password2_user.send_keys(valid_password2)
-        sleep(1)
         agree_user = driver.find_element_by_xpath('//*[@id="usr_terms_agree"]')
         agree_user.click()
         checkbox_user = driver.find_element_by_xpath('//*[@id="usr_data_manage"]')
         checkbox_user.click()
-        sleep(2)
         registration_user = driver.find_element_by_id("newUser_submit_button")
         registration_user.click()
-        sleep(3)
         WebDriverWait(driver, 3).until(EC.alert_is_present(), '')
         alert = driver.switch_to.alert
         assert alert.text == u'Potwierdzenie hasła musi być takie same jak hasło !'
         alert.accept()
         print("alert accepted")
         
-
     def tearDown(self):
         self.driver.quit()
 
--- projekt3.py
+++ projekt3.py
@@ -17,12 +17,10 @@ class test(unittest.TestCase):
         availabledress = dresswedding.find_elements_by_css_selector('.preview')
         pressbutton = availabledress[0].find_element_by_css_selector('a')
         pressbutton.click()
-        sleep(3)
         buttonBar = driver.find_element_by_css_selector(".rozmiary_div")
         availableSizes = buttonBar.find_elements_by_css_selector(".atts_6")
         sizeLink = availableSizes[0].find_element_by_css_selector('a')
         sizeLink.click()
-        sleep(3)
         selectedSize = driver.find_element_by_id("selectedSize")
         assert selectedSize.text == sizeLink.text
         cookies_info = driver.find_element_by_id("close-cookie-info")
@@ -31,16 +29,14 @@ class test(unittest.TestCase):
         addToBasket = driver.find_element_by_id("add2BasketBtn")
         driver.execute_script("window.scrollTo(0, 30)")
         addToBasket.click()
-        sleep(1)
         discount =driver.find_element_by_xpath('//*[@id="pcode"]')
         driver.execute_script("window.scrollTo(0, 300)")
         discount.click()
         discount.send_keys(invalid_discount)
-        sleep(1)
         activateBar = driver.find_element_by_css_selector(".kupon")
         activateBar2 = activateBar.find_element_by_css_selector(".button")
         activateBar2.click()
-        sleep(5)
+        sleep(3)
         modalcontent = driver.find_element_by_css_selector(".nyroModalIframe")
         frames = modalcontent.find_elements_by_css_selector("iframe")
         assert len(frames) == 1
@@ -48,7 +44,6 @@ class test(unittest.TestCase):
         driver.switch_to.frame(frame)
         error = driver.find_element_by_css_selector("h1")
         assert error.text == u"BŁĄD: KOD NIEPOPRAWNY"
-        sleep(3)
 
     def tearDown(self):
         self.driver.quit()
